7002cc3cf5cf9cdf8d1f38ddcf72399823810c3d
dima <Dima.aleks.94@mail.ru>
2021-06-15 22:07:35 +0300

    add Component Task

--- TodoList/src/Task.tsx
+++ TodoList/src/Task.tsx
@@ -4,15 +4,26 @@ import AddItemForm from "./AddItemForm";
 import EditableSpan from "./EdiatableSpan";
 import {Button, Checkbox, IconButton} from "@material-ui/core";
 import {Delete} from "@material-ui/icons";
-//2/38
-type TasksPropsType  = {
-   task: TaskType
 
+type TasksPropsType = {
+    task: TaskType
+    todolistId: string
+    changeTaskTitle: (taskID: string, newTitle: string, todoListID: string) => void
+    changeTaskStatus: (taskID: string, newIsDoneValue: boolean, todoListID: string) => void
+    removeTask: (taskID: string, todoListID: string) => void
 }
 
 const Task = React.memo((props: TasksPropsType) => {
+    const changeTaskTitle = (title: string) => props.changeTaskTitle(props.todolistId, title, props.task.id)
+    const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        props.changeTaskStatus(props.todolistId, e.currentTarget.checked, props.task.id)
+        const removeTask = () => {
+            props.removeTask(props.task.id, props.todolistId)
+        }
+    }
+
     return (
-        <li >
+        <li>
 
                 <span className={props.task.isDone ? 'is-done' : ''}>
                     <Checkbox
