506902b69585bee8e14af2d4d6b7dc3cd683be3e
dima <Dima.aleks.94@mail.ru>
2021-08-26 14:35:18 +0300

    add libl formik and react-router-dom, create component Login

--- TodoList/src/api/todolists-api.ts
+++ TodoList/src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': 'f98497de-3aa4-4fb0-ba69-3087854eec9e'
     }
 })
 
