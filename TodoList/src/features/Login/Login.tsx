1ac911221bde5c0325328bffd8e36e73743d16cd
dima <Dima.aleks.94@mail.ru>
2021-08-27 07:26:37 +0300

    add validate for Login, refactor code

--- TodoList/src/features/Login/Login.tsx
+++ TodoList/src/features/Login/Login.tsx
@@ -16,7 +16,21 @@ export const Login = () => {
             password: '',
             rememberMe: false
         },
-
+        validate: (values) => {
+            debugger
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'no password'
+            } else if (values.password.length > 4) {
+                errors.password = 'password is too long'
+            }
+            return errors;
+        },
         onSubmit: values => {
             alert(JSON.stringify(values));
             formik.resetForm();
@@ -42,11 +56,11 @@ export const Login = () => {
                         <TextField
                             label="Email"
                             margin="normal"
-
-                            name='email'
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            onBlur={formik.handleBlur}
+                            {...formik.getFieldProps('email')}
+                            // name='email'
+                            // onChange={formik.handleChange}
+                            // value={formik.values.email}
+                            // onBlur={formik.handleBlur}
                         />
                         {formik.touched.email && formik.errors.email &&
                         <div style={{color: 'red'}}>{formik.errors.email}</div>}
@@ -54,22 +68,14 @@ export const Login = () => {
                             type="password"
                             label="Password"
                             margin="normal"
-
-                            name='password'
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            onBlur={formik.handleBlur}
+                            {...formik.getFieldProps('password')}
                         />
                         {formik.touched.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
                         <FormControlLabel
                             label={'Remember me'}
                             control={<Checkbox
-
-                                name='rememberMe'
-                                onChange={formik.handleChange}
-                                checked={formik.values.rememberMe}
+                                {...formik.getFieldProps('rememberMe')}
                             />}
-
                         />
                         <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
                     </FormGroup>
